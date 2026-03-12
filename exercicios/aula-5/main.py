711c91a960d34c269cc574ca73d16128b5623372
MagoDaZueira <otaviogarciacapobianco@gmail.com>
2026-03-12 15:37:28 -0300

    página de login que salva cookies

--- exercicios/aula-5/main.py
+++ exercicios/aula-5/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Depends, Request, HTTPException, status, Cookie, Response
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
 
@@ -30,4 +30,29 @@ def create_user(user: User):
 def login_render(request: Request):
     return templates.TemplateResponse(
         request=request, name="login.html"
+    )
+
+
+@app.post("/login")
+def login(user: User, response: Response):
+    usuario_encontrado = None
+    for u in users:
+        if u.nome == user.nome:
+            usuario_encontrado = u
+            break
+
+    if not usuario_encontrado:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Usuário não Encontrado")
+    
+    if user.senha != usuario_encontrado.senha:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Senha Incorreta")
+
+    response.set_cookie(key="session_user", value=usuario_encontrado.nome)
+    return {"message": "Logado com sucesso"}
+
+
+@app.get("/home")
+def home_render(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="home.html"
     )
