4b94ff4b531ea01ed4b18a1e3d6bf17007c5c72a
MagoDaZueira <otaviogarciacapobianco@gmail.com>
2026-03-23 15:24:40 -0300

    modelos iniciais do banco de dados

---
+++ projeto/models.py
@@ -0,0 +1,22 @@
+from typing import List
+from sqlmodel import SQLModel, Field, Relationship
+from datetime import datetime
+
+
+class User(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    username: str
+    bio: str
+
+    attempts: List["Attempt"] = Relationship(back_populates="user")
+
+
+class Attempt(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    wpm: float
+    accuracy: float
+    date: datetime
+    duration: int
+    user_id: int = Field(foreign_key="user.id")
+
+    user: "User" = Relationship(back_populates="attempts")
