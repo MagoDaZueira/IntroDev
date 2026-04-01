66af942f55453e72262780f2364cb11220f718af
MagoDaZueira <otaviogarciacapobianco@gmail.com>
2026-03-31 23:34:28 -0300

    centraliza criação de sessions do bd

--- projeto/main.py
+++ projeto/main.py
@@ -36,13 +36,17 @@ def on_startup():
     create_db_and_tables()
 
 
-def get_active_user(session_user: Annotated[str | None, Cookie()] = None):
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+def get_active_user(session_user: Annotated[str | None, Cookie()] = None, session: Session = Depends(get_session)):
     if not session_user:
         raise HTTPException(status_code=401, detail="Not authenticated")
 
-    with Session(engine) as session:
-        user = get_user_by_name(session, session_user)
-        return user
+    user = get_user_by_name(session, session_user)
+    return user
 
 
 @app.exception_handler(HTTPException)
@@ -74,76 +78,75 @@ async def get_login(request: Request):
 
 
 @app.post("/login")
-async def post_login(username: str = Form(...), password: str = Form(...)):
-    with Session(engine) as session:
-        try:
-            user = get_user_by_name(session, username)
-        except HTTPException:
-            return HTMLResponse('<p>User not found</p>')
-        if not verify_password(password, user.password):
-            return HTMLResponse('<p>Incorrect password</p>')
+async def post_login(username: str = Form(...), password: str = Form(...), session: Session = Depends(get_session)):
+    try:
+        user = get_user_by_name(session, username)
+    except HTTPException:
+        return HTMLResponse('<p>User not found</p>')
+    if not verify_password(password, user.password):
+        return HTMLResponse('<p>Incorrect password</p>')
 
-        response = Response()
-        response.headers["HX-Redirect"] = "/"
-        response.set_cookie(key="session_user", value=user.username)
-        return response
+    response = Response()
+    response.headers["HX-Redirect"] = "/"
+    response.set_cookie(key="session_user", value=user.username)
+    return response
 
 
 @app.post("/user")
-async def create_user(username: str = Form(...), bio: str = Form(""), password: str = Form(...)):
-    with Session(engine) as session:
-        user_db = session.exec(select(User).where(User.username == username)).first()
-        if user_db:
-            return HTMLResponse('<p>Username already exists</p>')
+async def create_user(username: str = Form(...), bio: str = Form(""), password: str = Form(...), session: Session = Depends(get_session)):
+    user_db = session.exec(select(User).where(User.username == username)).first()
+    if user_db:
+        return HTMLResponse('<p>Username already exists</p>')
 
-        new_user = User(
-            username=username,
-            bio=bio,
-            password=hash_password(password)
-        )
+    new_user = User(
+        username=username,
+        bio=bio,
+        password=hash_password(password)
+    )
 
-        session.add(new_user)
-        session.commit()
+    session.add(new_user)
+    session.commit()
 
-        response = Response()
-        response.headers["HX-Redirect"] = "/login"
-        return response
+    response = Response()
+    response.headers["HX-Redirect"] = "/login"
+    return response
 
 
 @app.post("/attempt")
-async def add_attempt(attempt: Attempt):
+async def add_attempt(attempt: Attempt, session: Session = Depends(get_session)):
     if not verify_attempt(attempt):
         raise HTTPException(status_code=400, detail="Invalid attempt")
 
-    with Session(engine) as session:
-        session.add(attempt)
-        session.commit()
-        session.refresh(attempt)
-        return attempt
+    session.add(attempt)
+    session.commit()
+    session.refresh(attempt)
+    return attempt
 
 
 @app.get("/profile")
-async def private_user_page(request: Request, user: User = Depends(get_active_user)):
-    wpm = await get_user_wpm(user.username)
-    playtime = await get_total_time(user.username)
-    attempts = await get_user_attempts(user.username)
-    
+async def private_user_page(request: Request, user: User = Depends(get_active_user), session: Session = Depends(get_session)):
+    wpm = get_user_wpm(session, user.username)
+    playtime = get_total_time(session, user.username)
+    attempts = get_user_attempts(session, user.username)
+    attempt_count = get_attempt_count(session, user.id)
+
     user_info = {
         "username": user.username,
         "bio": user.bio,
         "wpm": wpm,
         "playtime": playtime,
-        "attempts": attempts
+        "attempts": attempts,
+        "attempt_count": attempt_count
     }
 
     return templates.TemplateResponse(request, "/layouts/profile.html", context={"user": user_info})
 
 
 @app.get("/user/{username}")
-async def public_user_page(request: Request, username: str):
-    user = await get_user_by_name(username)
-    wpm = await get_user_wpm(user.username)
-    playtime = await get_total_time(user.username)
+async def public_user_page(request: Request, username: str, session: Session = Depends(get_session)):
+    user = get_user_by_name(session, username)
+    wpm = get_user_wpm(session, user.username)
+    playtime = get_total_time(session, user.username)
     
     user_info = {
         "username": user.username,
@@ -162,72 +165,67 @@ def get_user_by_name(session: Session, username: str) -> User:
     return user
 
 
-@app.get("/user/{username}/attempts")
-async def get_user_attempts(username: str):
-    with Session(engine) as session:
-        user = get_user_by_name(session, username)
-        return user.attempts
+def get_attempt_count(session: Session, user_id: int):
+    statement = select(func.count()).where(Attempt.user_id == user_id)
+    return session.exec(statement).one()
 
 
-@app.get("/user/{username}/wpm")
-async def get_user_wpm(username: str):
-    with Session(engine) as session:
-        user = get_user_by_name(session, username)
-        query = select(func.max(Attempt.wpm)).where(Attempt.user_id == user.id)
-        return session.exec(query).first()
+def get_user_attempts(session: Session, username: str):
+    user = get_user_by_name(session, username)
+    return user.attempts
 
 
-@app.get("/user/{username}/playtime")
-async def get_total_time(username: str):
-    with Session(engine) as session:
-        user = get_user_by_name(session, username)
+def get_user_wpm(session: Session, username: str):
+    user = get_user_by_name(session, username)
+    query = select(func.max(Attempt.wpm)).where(Attempt.user_id == user.id)
+    return session.exec(query).first()
 
-        total = session.exec(
-            select(func.sum(Attempt.duration))
-            .where(Attempt.user_id == user.id)
-        ).first()
 
-        return total or 0
+def get_total_time(session: Session, username: str):
+    user = get_user_by_name(session, username)
+
+    total = session.exec(
+        select(func.sum(Attempt.duration))
+        .where(Attempt.user_id == user.id)
+    ).first()
+
+    return total or 0
 
 
 @app.patch("/user/{username}/bio")
-async def update_bio(username: str, bio: str):
-    with Session(engine) as session:
-        user = get_user_by_name(session, username)
-        user.bio = bio
-        session.add(user)
-        session.commit()
-        session.refresh(user)
-        return user
+async def update_bio(username: str, bio: str, session: Session = Depends(get_session)):
+    user = get_user_by_name(session, username)
+    user.bio = bio
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+    return user
 
 
 @app.patch("/user/{username}/username")
-async def update_username(username: str, new_username: str):
-    with Session(engine) as session:
-        user = get_user_by_name(session, username)
+async def update_username(username: str, new_username: str, session: Session = Depends(get_session)):
+    user = get_user_by_name(session, username)
 
-        user_new_name = session.exec(select(User).where(User.username == new_username)).first()
-        if user_new_name:
-            raise HTTPException(status_code=400, detail="Username already exists")
+    user_new_name = session.exec(select(User).where(User.username == new_username)).first()
+    if user_new_name:
+        raise HTTPException(status_code=400, detail="Username already exists")
 
-        user.username = new_username
-        session.add(user)
-        session.commit()
-        session.refresh(user)
-        return user
+    user.username = new_username
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+    return user
 
 
-async def delete_attempts(user_id: int):
-    with Session(engine) as session:
-        session.exec(delete(Attempt).where(Attempt.user_id == user_id))
-        session.commit()
+def delete_attempts(session: Session, user_id: int):
+    session.exec(delete(Attempt).where(Attempt.user_id == user_id))
+    session.commit()
 
 @app.delete("/user/{username}")
-async def delete_user(username: str):
-    with Session(engine) as session:
-        user = get_user_by_name(session, username)
-        await delete_attempts(user.id)
-        session.exec(delete(User).where(User.username == username))
-        session.commit()
+async def delete_user(username: str, session: Session = Depends(get_session)):
+    user = get_user_by_name(session, username)
+    delete_attempts(session, user.id)
+    session.exec(delete(User).where(User.username == username))
+    session.commit()
 
     return {"message": "User deleted successfully"}
