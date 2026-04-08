eabc9aebdd8215e4fa7c5c0b6e6fa9e17d237d7b
MagoDaZueira <otaviogarciacapobianco@gmail.com>
2026-04-08 20:57:48 -0300

    exercício aula 8

--- exercicios/aula-8/main.py
+++ exercicios/aula-8/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory=["templates"])
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 curtidas = 0
 
@@ -23,3 +25,27 @@ async def curtir(request: Request):
     global curtidas
     curtidas = 0
     return templates.TemplateResponse(request, "partials/contador.html", {"curtidas": curtidas})
+
+@app.get("/curtidas", response_class=HTMLResponse)
+async def get_curtidas(request: Request):
+    return templates.TemplateResponse(request, "curtidas.html", {"curtidas": curtidas})
+
+@app.get("/jupiter", response_class=HTMLResponse)
+async def get_jupiter(request: Request):
+    return templates.TemplateResponse(request, "aula-1/jupiter/index.html")
+
+@app.get("/professor", response_class=HTMLResponse)
+async def get_professor(request: Request):
+    return templates.TemplateResponse(request, "aula-1/site-professor/index.html")
+
+@app.get("/research", response_class=HTMLResponse)
+async def get_research(request: Request):
+    return templates.TemplateResponse(request, "aula-1/site-professor/research.html")
+
+@app.get("/students", response_class=HTMLResponse)
+async def get_students(request: Request):
+    return templates.TemplateResponse(request, "aula-1/site-professor/students.html")
+
+@app.get("/contact", response_class=HTMLResponse)
+async def get_contact(request: Request):
+    return templates.TemplateResponse(request, "aula-1/site-professor/contact.html")
